b493e5b56fec3c13bdf324d288970d9e0438b33b
cainiaoNO1 <2462875348@qq.com>
2024-06-14 18:24:14 +0800

    修改threejs组件主题监听

--- docs/.vitepress/theme/components/Three.vue
+++ docs/.vitepress/theme/components/Three.vue
@@ -4,11 +4,11 @@
   </div>
 </template>
 <script setup lang="ts">
-import { ref, nextTick, onMounted } from "vue"
+import { ref, nextTick, onMounted, watch } from "vue"
 import * as THREE from "three"
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js"
-import { useRoute } from "vitepress"
+import { useData } from "vitepress"
 
 const canvasDom = ref<HTMLElement>()
 
@@ -19,28 +19,32 @@ let scene: THREE.Scene,
   mixer: THREE.AnimationMixer,
   clock: THREE.Clock;
 
-const route = useRoute()
+const pageData = useData()
 
 onMounted(() => {
-  nextTick(() => {
-    const CW = canvasDom.value!.clientWidth, CH = canvasDom.value!.clientHeight;
-    scene = new THREE.Scene()
-    scene.background = new THREE.Color(0xffffff)
-    scene.environment = new THREE.Color(0xffffff)
-    camera = new THREE.PerspectiveCamera(50, CW / CH, 0.1, 1000);
-    camera.position.set(0, 0, 3)
-    camera.updateProjectionMatrix()
-    renderer = new THREE.WebGLRenderer()
-    renderer.setSize(CW, CH)
-    canvasDom.value?.appendChild(renderer.domElement)
-    clock = new THREE.Clock()
-    
-    // loadControls()
-    render()
-    setLight()
-    loadModel()
-  })
+  const CW = canvasDom.value!.clientWidth, CH = canvasDom.value!.clientHeight;
+  scene = new THREE.Scene()
+  setBackage(pageData.isDark.value)
+  camera = new THREE.PerspectiveCamera(50, CW / CH, 0.1, 1000);
+  camera.position.set(0, 0, 3)
+  camera.updateProjectionMatrix()
+  renderer = new THREE.WebGLRenderer()
+  renderer.setSize(CW, CH)
+  canvasDom.value?.appendChild(renderer.domElement)
+  clock = new THREE.Clock()
+  
+  // loadControls()
+  render()
+  setLight()
+  loadModel()
 })
+
+
+watch(() => pageData.isDark.value, (newVal) => {
+  setBackage(newVal)
+}, { deep: true })
+
+
 
 function loadModel() {
   const gltfLoader = new GLTFLoader()
@@ -87,11 +91,20 @@ function render() {
   requestAnimationFrame(render)
 
   renderer.render(scene, camera)
-  // controls.update()
 
   let mixerUpdateDelta = clock.getDelta();
 
   if(mixer) mixer.update(mixerUpdateDelta)
+}
+
+function setBackage(isDark: boolean) {
+  if (isDark) {
+    scene.background = new THREE.Color('#1b1b1f')
+    scene.environment = new THREE.Color('#1b1b1f')
+  } else {
+    scene.background = new THREE.Color(0xffffff)
+    scene.environment = new THREE.Color(0xffffff)
+  }
 }
 </script>
 <style scoped lang="css">
